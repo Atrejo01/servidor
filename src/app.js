816c811ca549413ab58f4c8b19502a50a8817626
ATrejo <apolinartrejo@utng.edu.mx>
2022-07-06 06:11:53 -0500

    commit 3

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express= require('express');
 const morgan= require('morgan');
 const app=express();
+const cors= require('cors');    //<<<---
 
 app.set('port',3000);//<---
 
+//app.use(cors({origin:"http://localhost:4200/"}));    //<<<---
+app.use(cors());    //<<<---
 app.use(morgan('dev'));
 app.use(express.json());//<---
 app.use(express.urlencoded({extended:false}));//<---
